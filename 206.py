c5d658ae66126547ca99ac8359c275b24c28facd
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2013-07-16 14:44:03 -0500

    python 3 print

--- 206.py
+++ 206.py
@@ -6,5 +6,5 @@ square_gen = (str(i ** 2) for i in xrange(1000000000, 1400000000, 10))
 even_digits = range(2, 17, 2)
 for i in square_gen:
     if all([i[even] == str(even / 2 + 1) for even in even_digits]):
-        print "{}'s square root is {}".format(i, sqrt(int(i)))
+        print("{}'s square root is {}".format(i, sqrt(int(i))))
         break
